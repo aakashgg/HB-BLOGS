f07a67b4252593a30655b7de3402f38f7aca7e13
Aakash Gupta <mynameaakash46@gmail.com>
2025-05-12 23:54:51 +0530

    backend url

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // Configure CORS to allow credentials and specify allowed origins
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://hb-blogs-1.onrender.com',
   credentials: true
 }));
 
