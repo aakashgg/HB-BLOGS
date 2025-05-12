92edefcefe4d7a613c0cdd187613f8af7cd35cbd
Aakash Gupta <mynameaakash46@gmail.com>
2025-05-12 23:18:27 +0530

    all done

--- FRONTEND/src/pages/Myposts.tsx
+++ FRONTEND/src/pages/Myposts.tsx
@@ -26,7 +26,7 @@ const MyCard: React.FC<MyCardProps> = ({ _id, image, title, summary, authorName,
             <div className="flex">
                 <div className="w-1/3">
                     <div className="image-container" style={{ maxHeight: '200px', overflow: 'hidden' }}>
-                        <img src={`http://localhost:4000/${image}`} alt={title} className="w-full h-full object-cover" />
+                        <img src={image} alt={title} className="w-full h-full object-cover" />
                     </div>
                 </div>
                 <div className="w-2/3 p-4">
